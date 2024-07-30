1587cfe4199b6a8db6eb9f817528b483f91c5641
Mahmoud Ahmed <110298603+mouda4d@users.noreply.github.com>
2024-07-30 19:44:14 +0300

    more features

--- TextToImage/alphabet to pictures python.py
+++ TextToImage/alphabet to pictures python.py
@@ -3,13 +3,32 @@
 # upper and lowercase letters give the same images, spaces and dots are supported.
 # if you type any other character that character won't be converted
 
-
 import shutil
 import string
 import os
+import requests
+import zipfile
+import io
+
+owner = 'mouda4d'
+repo = 'Alphabets'
+branch = 'main'
+folder = 'MESSAGE'
+# get my github repo that contains the alphabets images
+git_url = f'https://github.com/{owner}/{repo}/archive/refs/heads/{branch}.zip'
+response = requests.get(git_url)
 
 dict = {}
 i = 1
+
+# Check if the request was successful
+if response.status_code == 200:
+    # extract and save the files into memory instead of disk for efficiency
+    with zipfile.ZipFile(io.BytesIO(response.content)) as zip:
+        zip.extractall()
+    print("images retrieved successfully")
+else:
+    print("request failed")
 
 #loop over lower and uppercase methods and map them to numbers
 for char in string.ascii_lowercase + string.ascii_uppercase:
@@ -25,18 +44,22 @@ dict[' '] = i+1
 #getting user input
 text = input("Please enter the text you would like to convert to images: ")
 try:
-    shutil.rmtree('userinput')
+    shutil.rmtree(folder)
 except:
-    print("Creating folder: userinput")
-os.makedirs('userinput', exist_ok=True)
+    print(f"Creating folder: {folder}")
+os.makedirs(folder, exist_ok=True)
+
+unmatched = []
 i = 1
 # loop over each character in userinput, convert to an image, rename it then copy it to the folder
 for char in text:
     try:
-        source = f'{dict[char]}.jpg'
-        renamed = f'userinput/{i}.jpg'
+        source = os.path.join(os.getcwd(), f'{repo}-{branch}/{dict[char]}.jpg')
+        renamed = os.path.join(os.getcwd(), f'{folder}/{i}.jpg')
         shutil.copy(source, renamed)
         i+=1
     # this message will be displayed whenever a character isnt matched
     except:
-        print(f"{char} does not have a matching image, please try again.")
+        if char not in unmatched:
+            print(f"{char} does not have a matching image, and was skipped.")
+        unmatched.append(char)
